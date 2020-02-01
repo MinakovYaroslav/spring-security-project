f1d2b27c8ea9f65775fcb15f4f207911400f6a57
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2020-02-01 23:04:23 +0300

    Removed TODO; Added methods;

--- src/main/java/com/minakov/springsecurityproject/service/TeamService.java
+++ src/main/java/com/minakov/springsecurityproject/service/TeamService.java
@@ -10,7 +10,8 @@ import java.util.List;
 public interface TeamService {
 
     Team findById(Long teamId);
-    List<Team> findAll(); //TODO FIND ALL BY STATUS
-    Team save(Team team); //TODO CREATE IN LIQUIBASE DEFAULT BOOLEAN STATUS TRUE
-    //TODO CHECK destination.setUpdated(team.getUpdated());
+    List<Team> findAll();
+    Team save(Team team);
+    List<Team> findAllFetchUsers();
+    Team findByIdFetchUsers(Long teamId);
 }
