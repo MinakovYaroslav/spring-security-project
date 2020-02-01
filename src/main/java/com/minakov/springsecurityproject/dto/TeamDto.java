e877bed76e584b6b82eeaa1a7cf5c88eeb507032
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2020-02-01 21:59:44 +0300

    Changed field List<Long> usersId from List<UserDto> users; Added methods;

--- src/main/java/com/minakov/springsecurityproject/dto/TeamDto.java
+++ src/main/java/com/minakov/springsecurityproject/dto/TeamDto.java
@@ -2,6 +2,7 @@ package com.minakov.springsecurityproject.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.minakov.springsecurityproject.model.Team;
+import com.minakov.springsecurityproject.model.User;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,21 +19,42 @@ import java.util.stream.Collectors;
 public class TeamDto extends AbstractDto {
 
     private String name;
-    private List<UserDto> users;
+    private List<Long> usersId;
 
     @Builder
-    public TeamDto(Long id, String name, List<UserDto> users) {
+    public TeamDto(Long id, String name, List<Long> usersId) {
         super(id);
         this.name = name;
-        this.users = users;
+        this.usersId = usersId;
+    }
+
+    public Team toEntity() {
+        return Team.builder()
+                .name(name)
+                .users(usersId.stream()
+                        .map(userId -> User.builder()
+                                .id(userId)
+                                .build())
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    public Team toEntity(Team team) {
+        team.setName(name);
+        team.setUsers(usersId.stream()
+                .map(userId -> User.builder()
+                        .id(userId)
+                        .build())
+                .collect(Collectors.toList()));
+        return team;
     }
 
     public static TeamDto toDto(Team team) {
         return TeamDto.builder()
                 .id(team.getId())
                 .name(team.getName())
-                .users(team.getUsers().stream()
-                        .map(UserDto::toDto)
+                .usersId(team.getUsers().stream()
+                        .map(User::getId)
                         .collect(Collectors.toList()))
                 .build();
     }
