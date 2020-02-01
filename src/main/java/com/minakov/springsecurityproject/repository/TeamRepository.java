e754c0da775c794da5fa221ba4e1f62c53d6f4e4
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2020-02-01 22:52:49 +0300

    Added methods;

--- src/main/java/com/minakov/springsecurityproject/repository/TeamRepository.java
+++ src/main/java/com/minakov/springsecurityproject/repository/TeamRepository.java
@@ -2,11 +2,21 @@ package com.minakov.springsecurityproject.repository;
 
 import com.minakov.springsecurityproject.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Yaroslav Minakov
  */
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    @Query("FROM Team t JOIN FETCH t.users")
+    List<Team> findAllFetchUsers();
+
+    @Query("FROM Team t JOIN FETCH t.users WHERE t.id =:teamId")
+    Team findByIdFetchUsers(@Param("teamId") Long teamId);
 }
