61550564ab6fc08d900254205127942f1c1ec9ec
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2020-02-01 22:54:33 +0300

    Changed UserDto from AdminUserDto; Small fixes;

--- src/main/java/com/minakov/springsecurityproject/rest/AdminRestControllerV1.java
+++ src/main/java/com/minakov/springsecurityproject/rest/AdminRestControllerV1.java
@@ -1,6 +1,6 @@
 package com.minakov.springsecurityproject.rest;
 
-import com.minakov.springsecurityproject.dto.admin.AdminUserDto;
+import com.minakov.springsecurityproject.dto.UserDto;
 import com.minakov.springsecurityproject.model.User;
 import com.minakov.springsecurityproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,17 +27,27 @@ public class AdminRestControllerV1 {
     }
 
     @PutMapping(value = "users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AdminUserDto> updateUser(@RequestBody @Valid AdminUserDto adminUserDto, @PathVariable("id") Long userId) {
+    public ResponseEntity<UserDto> updateUser(@RequestBody @Valid UserDto userDto, @PathVariable("id") Long userId) {
 
-        if (adminUserDto == null || userId == null) {
+        if (userDto == null || userId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!userDto.getId().equals(userId)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        User user = this.userService.findById(userId);
+
+        if (user == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
         HttpHeaders headers = new HttpHeaders();
-        User user = adminUserDto.toEntity();
+        user = userDto.toEntity(user);
 
         this.userService.save(user);
 
-        return new ResponseEntity<>(adminUserDto, headers, HttpStatus.OK);
+        return new ResponseEntity<>(userDto, headers, HttpStatus.OK);
     }
 }
