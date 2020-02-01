c1d039bf455c89b9ef7691c8dd8282cc18397015
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2020-02-01 21:55:53 +0300

    Changed field List<Long> projectsId from List<ProjectDto> projects; Added methods;

--- src/main/java/com/minakov/springsecurityproject/dto/CustomerDto.java
+++ src/main/java/com/minakov/springsecurityproject/dto/CustomerDto.java
@@ -2,6 +2,7 @@ package com.minakov.springsecurityproject.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.minakov.springsecurityproject.model.Customer;
+import com.minakov.springsecurityproject.model.Project;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,23 +19,44 @@ import java.util.stream.Collectors;
 public class CustomerDto extends AbstractDto {
 
     private String name;
-    private List<ProjectDto> projects;
+    private List<Long> projectsId;
 
     @Builder
     public CustomerDto(Long id,
                        String name,
-                       List<ProjectDto> projects) {
+                       List<Long> projectsId) {
         super(id);
         this.name = name;
-        this.projects = projects;
+        this.projectsId = projectsId;
+    }
+
+    public Customer toEntity() {
+        return Customer.builder()
+                .name(name)
+                .projects(projectsId.stream()
+                        .map(projectId -> Project.builder()
+                                .id(projectId).build())
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    public Customer toEntity(Customer customer) {
+        customer.setName(name);
+        customer.setProjects(projectsId.stream()
+                .map(projectId -> Project.builder()
+                        .id(projectId)
+                        .build())
+                .collect(Collectors.toList()));
+
+        return customer;
     }
 
     public static CustomerDto toDto(Customer customer) {
         return CustomerDto.builder()
                 .id(customer.getId())
                 .name(customer.getName())
-                .projects(customer.getProjects().stream()
-                        .map(ProjectDto::toDto)
+                .projectsId(customer.getProjects().stream()
+                        .map(Project::getId)
                         .collect(Collectors.toList()))
                 .build();
     }
